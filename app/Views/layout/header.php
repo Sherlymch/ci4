522abe71f58b7a5413be7dabf250fc9e4ae0a50a
sherlymch <sherlymch@gmail.com>
2021-03-03 16:03:54 +0700

    update

---
+++ app/Views/layout/header.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/header.css">
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <div class="logo">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/e/eb/LOGO_KABUPATEN_SEMARANG.png" alt="" width="80" height="100" style="float: left;">
+                <p style="width: 300px; padding-left:100px;">Pemerintah Kabupaten Semarang Kantor Kesatuan Bangsa dan Politik</p>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup" style="margin-left: 100px; margin-top: 40px">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    <a class="nav-link" href="/pages/pendaftaran">Pendaftaran</a>
+                    <a class="nav-link" href="/pages/aduanormas">Aduan Ormas</a>
+                    <a class="nav-link" href="/pages/dataperkecamatan">Data Perkecamatan</a>
+                    <a class="nav-link" href="/pages/legalitas">Legalitas</a>
+                    <a class="nav-link" href="/pages/login" style="margin-left: 150px;">Login</a>
+                </div>
+            </div>
+        </div>
+    </nav>
---
+++ app/Views/pages/beranda.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>Document</title>
+
+
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/e/eb/LOGO_KABUPATEN_SEMARANG.png" alt="" width="100" height="120">
+            <p style="padding-right: 200px; padding-left: 20px">Pemerintahan Kabupaten Semarang</p>
+
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav" style="padding-right: 650px; padding-top:50px">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pendaftaran</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Aduan Ormas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Data Perkecamatan</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Legalitas</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+</body>
+
+</html>
